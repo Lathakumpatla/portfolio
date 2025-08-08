3355a42e58e10af96091e666ab6c4acd4c34f4e8
Latha <kumpatlalatha3@gmail.com>
2025-08-08 16:28:23 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
-import type { NextConfig } from "next";
-
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  
 };
-export default nextConfig;
 
-
+module.exports = nextConfig;
